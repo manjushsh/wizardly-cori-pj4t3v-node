030f0323212898a43bbd644891c589776245a21d
_ <94426452+manjushsh@users.noreply.github.com>
2024-01-04 04:35:19 +0000

    Update 2 files

--- services/constants.js
+++ services/constants.js
@@ -5,6 +5,11 @@ const Constants = {
   STABILITYAI_1_5: "runwayml/stable-diffusion-v1-5",
   STABILITYAI_UD_1_5: "stablediffusionapi/sdxl-unstable-diffusers-y",
   STABILITYAI_2_1: "stabilityai/stable-diffusion-2-1",
+  MICROSOFT_PHI2: "microsoft/phi-2",
+  TINYLLMA1_1B_CHAT_V1_0: "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+
+  DEFAULT_INSTRUCTION: `<|system|>
+  You are a chatbot who can help extract data from given HTML sanitized string!</s>`,
 };
 
 module.exports = Constants;
